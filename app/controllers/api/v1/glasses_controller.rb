5d2ecd64877ac2be1606eacf73440ee2ae40cd6e
Francesca Santoriello <f.santoriello@hotmail.com>
2023-09-25 15:31:27 +0200

    serve photo urls

--- app/controllers/api/v1/glasses_controller.rb
+++ app/controllers/api/v1/glasses_controller.rb
@@ -1,13 +1,34 @@
 class Api::V1::GlassesController < ApplicationController
     def index
-        @glasses = Glass.all
-
-        render json: @glasses
+        glasses = Glass.all
+        response_data = glasses.map { |glass| set_data(glass) }
+        
+        render json: response_data
     end
     
     def show
-        @glass = Glass.find(params[:id])
+        glass = Glass.find(params[:id])
+        response_data = set_data(glass)
+        
+        render json: response_data
+    end
 
-        render json: @glass
+    private
+
+    def set_data(glass)
+        {
+            id: glass.id,
+            sku: glass.sku,
+            name: glass.name,
+            brand: glass.brand,
+            price: glass.price,
+            color: glass.color,
+            frame: glass.frame,
+            material: glass.material,
+            measure: glass.measure,
+            woman: glass.woman,
+            man: glass.man,
+            photos: glass.photos.map { |p| p.url }
+        }
     end
 end
